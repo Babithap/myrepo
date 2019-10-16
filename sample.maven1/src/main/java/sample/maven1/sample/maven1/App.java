d876985e2664872c6875bdeaa48f71f89fa00c5a
Babithap <55275616+Babithap@users.noreply.github.com>
2019-10-16 12:15:08 +0530

    Add files via upload

---
+++ sample.maven1/src/main/java/sample/maven1/sample/maven1/App.java
@@ -0,0 +1,16 @@
+package sample.maven1.sample.maven1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public int i ;
+    public static void main( String[] args )
+    {
+    	
+        System.out.println( "Hello World!" );
+        System.out.println(10/5);
+    }
+}
